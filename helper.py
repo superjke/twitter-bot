55af073d8da639ef8c986e40397492c12cbb8926
superjke <superjke@noemail.com>
2019-10-01 17:24:53 +0100

    Added printSessionSummary()

--- helper.py
+++ helper.py
@@ -7,12 +7,10 @@ def check_follow_limit(followed, limit):
     return False
 
 
-def wait_random_time(waitLong):
-    if waitLong:
-        i = random.randint(20,60)
-        print("Waiting for {} seconds".format(i))
-        time.sleep(i)
-    else:
-        i = random.randint(3,6)
-        print("Waiting for {} seconds".format(i))
-        time.sleep(i)
+def wait_random_time():
+    i = random.randint(3,6)
+    time.sleep(i)
+
+
+def printSessionSummary(followed, liked):
+    print("{} users followed and {} tweets liked so far in this session".format(followed, liked))
